d77c7bf6fa7b4760a9a1d693a5e9010de409f58a
elyas74 <egh1374@gmail.com>
2016-05-14 22:32:42 +0430

    better logging

--- mini_logger.js
+++ mini_logger.js
@@ -8,9 +8,12 @@ var fs = require('fs');
 var path = require('path');
 
 function log(log_string, host) {
+
     var d = new Date();
 
     var date = d.getFullYear() + '/' + d.getMonth() + '/' + d.getDate();
+
+    var file_name = d.getFullYear() + '-' + d.getMonth() + '-' + d.getDate();
 
     var second = d.getSeconds() < 10 ? '0' + d.getSeconds() : d.getSeconds();
     var minuate = d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes();
@@ -19,18 +22,16 @@ function log(log_string, host) {
     var log_date = "[" + date + " " + time + "]";
 
     console.log(log_date + " -> " + log_string);
-    file_log(log_date + " -> " + log_string + '\n', host);
+
+    file_log(log_date + " -> " + log_string + '\n', file_name);
 }
 
 module.exports = log;
 
-function file_log(log, host) {
-    fs.appendFile(path.join(__dirname, "./server.log"), log, function(err) {
-        if (err) return console.error(err);
-    });
+function file_log(log, file_name) {
 
-    if (host)
-        fs.appendFile(path.join(__dirname, "./server-" + host + ".log"), log, function(err) {
-            if (err) return console.error(err);
-        });
+    fs.appendFile(path.join(__dirname, "./server-" + file_name + ".log"), log, function(err) {
+        if (err)
+            console.error(err);
+    });
 }
